957f25210b42170e9f26ac43faff2ccd07a25a7e
Samuel Abolo <ikabolo59@gmail.com>
2024-10-05 17:45:58 +0100

    Error code for resource not found added

--- src/shared/errors.py
+++ src/shared/errors.py
@@ -2,12 +2,15 @@
     Error classes for this module
 """
 
+from typing import Optional
 from enum import IntEnum
 
 
 class ErrorCodes(IntEnum):
+    """Error codes supported by the application"""
 
     GENERIC_CODE = 100
+    RESOURCE_NOT_FOUND = 404
 
 
 class CardboardGenericException(Exception):
@@ -19,7 +22,7 @@ class CardboardGenericException(Exception):
     error_code: int = ErrorCodes.GENERIC_CODE.value
     error_message: str = "An Error has occurred in the system"
 
-    def __init__(self, error_details):
+    def __init__(self, error_details: Optional[dict] = None):
 
-        self.error_details = error_details
+        self.error_details = error_details or {}
         super().__init__(error_details)
