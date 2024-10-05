1925224bc57e962dbf0865dd47cbc5c7e4f45577
Samuel Abolo <ikabolo59@gmail.com>
2024-10-05 17:38:38 +0100

    Persistence repositories

---
+++ src/shared/repo/base.py
@@ -0,0 +1,30 @@
+from typing import Any, Optional, Iterable
+from abc import ABC, abstractmethod
+
+
+class AbstractDomainEntityPersistenceRepository(ABC):
+    """Abstraction for persisting domain entities"""
+
+    @abstractmethod
+    def get(self, uid: Any) -> Optional[Any]:
+
+        """Get a domain entity by it's ID"""
+
+    @abstractmethod
+    def fetch_all(self, query: Any) -> Iterable[Any]:
+        """Get a list of domain entities based on a filter"""
+
+    @abstractmethod
+    def save(self, entity: Any) -> Any:
+        """
+        Persist the state of the entity's attributes.
+
+        If the domain entity did not exist, create it.
+        """
+
+    @abstractmethod
+    def delete(self, entity: Any) -> bool:
+
+        """
+        Delete a domain entity from persistence layer
+        """
